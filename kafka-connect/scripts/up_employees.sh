18e5e599755e73484fe84545aaac01350c6be49d
Gabriel Solano <gabriel.solano@gorillalogic.com>
2019-08-13 23:32:04 -0600

    more config added to connectors

--- kafka-connect/scripts/up_employees.sh
+++ kafka-connect/scripts/up_employees.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 CT="Content-Type:application/json"
 
-
-
 curl -H $CT -d "@samples.employees.source.json" -X POST http://localhost:8083/connectors
-#curl -H $CT -d "@samples.employees.target.json" -X POST http://localhost:8083/connectors
-
-
-
-
+curl -H $CT -d "@samples.employees.target.json" -X POST http://localhost:8083/connectors
